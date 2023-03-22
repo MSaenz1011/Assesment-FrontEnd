2da14d8bd4d224886e5513868dce68ba71b95e41
MSaenz1011 <michaelsaenz96@hotmail.com>
2023-03-22 00:43:42 -0500

    add ons

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./productcard.css";
+
+export default function ProductCard(products) {
+  return (
+    <article className='product-card'>
+      <div className='product-picture'>
+        <img src='' alt='' />
+      </div>
+      <div className='product-info'>
+        <p></p>
+      </div>
+    </article>
+  );
+}
--- src/pages/Home-Page/Home.jsx
+++ src/pages/Home-Page/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./home.css";
+import ProductCard from "../../components/ProductCard";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
@@ -10,5 +11,19 @@ export default function Home() {
     axios.get(URL).then((res) => setProducts(res.data.results));
   }, []);
 
-  return <div></div>;
+  return (
+    <>
+      <div class='flip-card'>
+        <div class='flip-card-inner'>
+          <div class='flip-card-front'>
+            <h2>You Better Flip Me</h2>
+          </div>
+          <div class='flip-card-back'>
+            <h2>Check the products before the timer ends</h2>
+          </div>
+        </div>
+      </div>
+      <ProductCard product={products} />
+    </>
+  );
 }
